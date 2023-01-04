f68900840a314acee0b99706b061d5141b15b1c1
MuhammadAjiSolehudin <muhammadajisolehudin@gmail.com>
2023-01-04 13:12:49 +0700

    perbaikan migrasi users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,16 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->integer('id_user');
+            $table->integer('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('roles_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->rememberToken();
             $table->timestamps();
-
-            $table->primary('id_user');
 
            
 
